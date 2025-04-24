44e89fb424ceaf637a2d753e0cfbdfa278138ecf
saadhn4 <saadcfi4@gmail.com>
2025-04-24 14:12:06 +0400

    done with app

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const App = () => {
+  const [advice, setAdvice] = useState(null);
+  const getAdvice = async () => {
+    try {
+      const response = await axios.get("https://api.adviceslip.com/advice");
+      setAdvice(response.data);
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  };
+
+  return (
+    <div className="h-[90vh] flex flex-col justify-center items-center bg-gray-50 px-4">
+      <h1 className="text-2xl font-bold mb-4">Advice Generator</h1>
+      <button
+        onClick={getAdvice}
+        className="bg-blue-400 text-white font-bold rounded-lg p-1.5 px-2 cursor-pointer mb-3"
+      >
+        Generate
+      </button>
+      <div className="text-center bg-white rounded-lg shadow p-4">
+        <p className="font-semibold">"{advice && advice.slip.advice}"</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
